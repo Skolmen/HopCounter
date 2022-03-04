92403c5953cae8230e430c0ec3a6fda7374dec13
Skolmen <taxen123@hotmail.se>
2022-03-04 15:44:46 +0100

    finds contenttype for dragalong

---
+++ ContentType.py
@@ -0,0 +1,40 @@
+from functools import total_ordering
+import subprocess
+
+def main():
+    inputVals = inputs()
+    #inputVals['path'] = "C:\Users\Jon\OneDrive - Linköpings universitet\Kurser\TDTS11 - Datornät och internetprotokoll\Laborationer\Assignment 4\task 3\Question 2\2. xinhuanet.com\trace_xihuanet.com.pcapng"
+    allcontent = sharker(inputVals['path'], inputVals['tcpStream'])
+    allcontent = getContentType(allcontent)
+
+    print("The drag along content types for the trace are:")
+    for i in allcontent:
+        print(i)
+
+    print(getContentType(allcontent))
+
+def sharker(path, tcpStream):
+
+    cmdTshark = ["tshark", "-r", path, '-T', 'fields', '-e', 'http.content_type', '-Y', 'http.content_type && tcp.stream eq '+ tcpStream]
+
+    contentTypes = subprocess.check_output(cmdTshark, shell=True)
+
+    contentTypes = contentTypes.split(b'\r\n')
+    contentTypes = [i.decode('utf-8') for i in contentTypes]
+    contentTypes = list(filter(None, contentTypes))
+
+    return contentTypes
+
+def getContentType(allContent):
+    return list(dict.fromkeys(allContent))
+
+def inputs():
+    path = input("Path to tracefile: ")
+    tcpStream = input("TCP-stream id of the inital request: ")
+    return {
+        "path": path,
+        "tcpStream": tcpStream
+    }
+
+
+main()
